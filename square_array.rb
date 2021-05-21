753f5a266cb362dde8342c19ff22db457d67d61c
Sean Ngo <seanzngo@gmail.com>
2021-05-20 22:51:23 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
-end
+  #your code here
+  sq = []
+  array.each { |arr| sq << arr**2}
+  sq
+
+  # sqd_arr = array.each { |arr| }
+  # sqd_arr**2
+  # sqd_arr
+
+  end
